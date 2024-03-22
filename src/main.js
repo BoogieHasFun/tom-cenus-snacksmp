362eec8308eb7ed432b75faf1b27ac0bcaa772ac
boogie <76754631+BoogieHasFun@users.noreply.github.com>
2024-03-22 13:50:04 -0400

    dfsdfsdsdfsdfs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // just grab a DOM element
 var cenusimg = document.getElementById('cenus-img')
-panzoom(cenusimg)
+panzoom(cenusimg, {
+    bounds: true,
+    boundsPadding: 0.001    
+  });
 
 //bls
